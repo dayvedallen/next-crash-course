2bcd78dcaad4b090f1c70bdf60ea8cb0a524397c
dayved <dayvedallen@gmail.com>
2024-04-19 15:18:41 -0400

    nvmrc

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,5 +1,5 @@
-import ArticleItem from './ArticleItem'
-import articleStyles from '../styles/Article.module.css'
+import ArticleItem from "./ArticleItem";
+import articleStyles from "../styles/Article.module.css";
 
 const ArticleList = ({ articles }) => {
   return (
@@ -8,7 +8,9 @@ const ArticleList = ({ articles }) => {
         <ArticleItem article={article} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ArticleList
+export default ArticleList;
+// Assign a unique key to each element: Within the .map() callback function, ensure that each element you return has a key prop. The value of the key should be unique for each element.
+// Choosing a Key Value: The best practice for key values is to use unique and stable identifiers. If your data includes a unique ID (like a database ID), use that. If not, you can use the item index as a last resort, but be aware that using indexes can negatively affect performance and cause issues with component state, especially if the list can change order.
